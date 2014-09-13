6ab0b6052884cd56a4ad8482be8b011bdc302090
Victor Bravo <airthbravo@gmail.com>
2014-09-13 19:02:04 -0400

    Cars and Users ( users )
    i made some users.

--- objects/Producer.java
+++ objects/Producer.java
@@ -3,42 +3,20 @@ package objects;
 public class Producer {
 
 	
-	public Car[] makeCars(){
-		Car[] cars = new Car[30];
+	public User[] makeUsers(){
+		User[] users = new User[10];
 		
-		
-		/* mpg, numDoors, year, price, fuelType, make, model, carType*/
-		cars[0] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[1] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[2] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[3] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[4] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[5] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[6] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[7] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[8] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[9] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[10] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[11] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[12] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[13] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[14] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[15] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[16] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[17] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[18] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[19] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[20] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[21] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[22] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[23] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[24] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[25] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[26] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[27] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[28] = new Car(0, 0, 0, 0, null, null, null, null);
-		cars[29] = new Car(0, 0, 0, 0, null, null, null, null);
-		
-		return cars;
+		/* travel distance, time spent driving, budget, username, current vehicle */
+		users[0] = new User(34, 91, 20000, "user1", null);
+		users[1] = new User(10, 21, 15000, "user2", null);
+		users[2] = new User(40, 110, 45000, "user3", null);
+		users[3] = new User(20, 50, 35000, "user4", null);
+		users[4] = new User(24, 45, 40000, "user5", null);
+		users[5] = new User(50, 125, 50000, "user6", null);
+		users[6] = new User(5, 15, 10000, "user7", null);
+		users[7] = new User(30, 72, 20000, "user8", null);
+		users[8] = new User(15, 35, 26000, "user9", null);
+		users[9] = new User(45, 85, 32000, "user10", null);
+		return users;
 	}
 }
