697e860e396778cbd881b75cc1413e5cb4e8698a
wootastic <rbaxter.08@gmail.com>
2014-09-13 23:49:07 -0400

    Added a method to user
    which compares him to another user and gives a score based on how
    similar they are.  0 being most similar 300 being least similar.

--- objects/Producer.java
+++ objects/Producer.java
@@ -7,16 +7,16 @@ public class Producer {
 		User[] users = new User[10];
 		
 		/* travel distance, time spent driving, budget, username, current vehicle */
-		users[0] = new User(34, 91, 20000, "user1", null);
-		users[1] = new User(10, 21, 15000, "user2", null);
-		users[2] = new User(40, 110, 45000, "user3", null);
-		users[3] = new User(20, 50, 35000, "user4", null);
-		users[4] = new User(24, 45, 40000, "user5", null);
-		users[5] = new User(50, 125, 50000, "user6", null);
-		users[6] = new User(5, 15, 10000, "user7", null);
-		users[7] = new User(30, 72, 20000, "user8", null);
-		users[8] = new User(15, 35, 26000, "user9", null);
-		users[9] = new User(45, 85, 32000, "user10", null);
+		users[0] = new User(34, 91, 20000, "user1", null, 0);
+		users[1] = new User(10, 21, 15000, "user2", null, 0);
+		users[2] = new User(40, 110, 45000, "user3", null, 1);
+		users[3] = new User(20, 50, 35000, "user4", null, 1);
+		users[4] = new User(24, 45, 40000, "user5", null, 1);
+		users[5] = new User(50, 125, 50000, "user6", null, 1);
+		users[6] = new User(5, 15, 10000, "user7", null, 2);
+		users[7] = new User(30, 72, 20000, "user8", null, 2);
+		users[8] = new User(15, 35, 26000, "user9", null, 0);
+		users[9] = new User(45, 85, 32000, "user10", null, 0);
 		return users;
 	}
 }
--- objects/User.java
+++ objects/User.java
@@ -3,6 +3,7 @@ package objects;
 import java.util.HashMap;
 
 public class User {
+	private int environment; //0 = city 1 = suburban 2 = rural 
 	private int travelDistance;
 	private int timeSpentDriving;
 	private double budget;
@@ -11,8 +12,9 @@ public class User {
 	private HashMap<Car, Integer> pastPurchases;
 
 
-	public User(int travelDistance, int timeSpentDriving, double budget, 
-			String username, Car currentVehicle){
+	public User(int travelDistance, int timeSpentDriving, int budget, 
+			String username, Car currentVehicle, int environment){
+		this.environment = environment;
 		this.travelDistance = travelDistance;
 		this.timeSpentDriving = timeSpentDriving;
 		this.budget = budget;
@@ -66,6 +68,54 @@ public class User {
 
 	public void setCurrentVehicle(Car currentVehicle){
 		this.currentVehicle = currentVehicle;
+	}
+	
+	/**
+	 * compares two users and returns a score based on how well they match.
+	 * A close match is a 0 and the worst possible match is 300.
+	 * @param other the other user we are comparing to
+	 * @return int score between 0 and 300
+	 */
+	public int matchUser(User other){
+		int score = 0;
+		int distanceDiff;
+		int timeDiff;
+		
+		//gets environment score
+		if(this.environment != other.environment){
+			if(Math.abs((this.environment - other.environment)) == 1){
+				score += 50;
+			}else{
+				score += 100;
+			}
+		}
+		
+		//gets travelDistance score
+		if(this.travelDistance>other.travelDistance){
+			distanceDiff = this.travelDistance - other.travelDistance;
+		}else{
+			distanceDiff = other.travelDistance - this.travelDistance;
+		}
+		if(distanceDiff > 100){
+			score+=100;
+		}else{
+			score+=distanceDiff;
+		}
+		
+		//gets timeDiff score
+		if(this.timeSpentDriving>other.timeSpentDriving){
+			timeDiff = this.timeSpentDriving - other.travelDistance;
+		}else{
+			timeDiff = other.timeSpentDriving - this.timeSpentDriving;
+		}
+		if(timeDiff > 100){
+			score+=100;
+		}else{
+			score+=timeDiff;
+		}
+		
+		return score;
+		
 	}
 
 	
